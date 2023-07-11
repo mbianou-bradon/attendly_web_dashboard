62bcfd2597f7c3ad3cc4de4d4cba497c7a8cc1c4
mbianou-bradon <mbianoubradon2000@gmail.com>
2023-07-11 10:58:13 +0100

    handled secured routes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,20 +3,25 @@ import { MdEmail, MdLock } from "react-icons/md"
 import { BiSolidSchool } from "react-icons/bi"
 import { IoIosEyeOff, IoIosEye } from "react-icons/io"
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
 
 export default function Login() {
+    /**Stat management */
     const [userMatricule, setUserMatricule] = React.useState<string>("");
     const [userPassword, setUserPassword] = React.useState<string>("");
     const [isPasswordVisible, setIsPasswordVisible] = React.useState<boolean>(false)
     const [error, setError] = React.useState<boolean>(false);
     const [errorMessage, setErrorMessage] = React.useState<any>("An error occured");
 
+    /**utility functions */
+    const navigate = useNavigate()
 
     React.useEffect(()=>{
         const jwtToken = JSON.parse(localStorage.getItem("@jwtToken") as string);
-        if(jwtToken){}
-            // router.push("/");
+        if(jwtToken){
+            navigate("/");
+        }
     },[]);
 
     let type = "password";
@@ -58,31 +63,31 @@ export default function Login() {
     return(
         <div className="absolute top-0 right-0 w-full h-full bg-gradient-to-tr from-primary to-purple-800">
             <div className="flex items-center justify-center h-full w-full">
-                <div className="border-primary flex flex-col">
+                <div className="border-neutral_white flex flex-col">
                     <div className="mb-10 flex justify-center w-[8rem] mx-auto">
                         <img src={logoWhite} alt={"Attendly Logo"} />
                     </div>
                     {error && <div className="py-4 bg-red-600 rounded-md text-white font-bold text-center text-sm">{errorMessage}</div>}
                     <form onSubmit={handleSignInUser}>
                         <div className="py-2 flex flex-col gap-10 text-white">
-                            <div className="flex items-center gap-4 py-2 border border-slate-800 px-4 h-14 rounded-md">
+                            <div className="flex items-center gap-4 py-2 border border-neutral_white px-4 h-14 rounded-md">
                                 <div>
                                     <BiSolidSchool className="text-xl"/>
                                 </div>
-                                <input type="text" placeholder="Matriculation Number" value={userMatricule} onChange={(matricule)=>setUserMatricule(matricule.target.value)} className="h-full pl-2 focus:outline-none focus:ring-0 rounded border border-slate-800 bg-transparent text-sm" required/>
+                                <input type="text" placeholder="Matriculation Number" value={userMatricule} onChange={(matricule)=>setUserMatricule(matricule.target.value)} className="h-full pl-2 focus:outline-none focus:ring-0 rounded border border-neutral_white bg-transparent text-sm" required/>
                             </div>
 
-                            <div className="flex items-center gap-4 py-2 border border-slate-800 px-4 h-14 rounded-md">
+                            <div className="flex items-center gap-4 py-2 border border-neutral_white px-4 h-14 rounded-md">
                                 <div>
                                     <MdLock className="text-xl"/>
                                 </div>
-                                <input type={type} placeholder="Password" value={userPassword} onChange={(password)=>setUserPassword(password.target.value)} className="h-full pl-2 focus:outline-none focus:ring-0 rounded border border-slate-800 bg-transparent text-sm" required/>
+                                <input type={type} placeholder="Password" value={userPassword} onChange={(password)=>setUserPassword(password.target.value)} className="h-full pl-2 focus:outline-none focus:ring-0 rounded border border-neutral_white bg-transparent text-sm" required/>
                                 <div className="text-xl cursor-pointer" onClick={handleViewPassword}>
                                     {isPasswordVisible? <IoIosEye/> : <IoIosEyeOff/>}
                                 </div>
                             </div>
                             
-                            <button type="submit" className="border-secondary text-white bg-secondary py-2 rounded-md hover:bg-secondary/90 select-none">LOGIN</button>
+                            <button type="submit" className="border-secondary text-white bg-secondary py-2 rounded-md hover:bg-secondary/90 select-none active:scale-95">LOGIN</button>
                         </div>
                         
                     </form>
